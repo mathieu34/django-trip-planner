6dd59b0a88912462d4b6a4a422dd535f8dc19c41
Navid SABETE <nevid.dev@gmail.com>
2026-08-04 17:20:49 +0200

    feat: integrate config settings for TripAdvisor API integration

--- findmyattractions_backend/findmyattractions_backend/settings.py
+++ findmyattractions_backend/findmyattractions_backend/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import environ, os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$0d_1ca$my=^@749t*(v7c!zy6xq)+4h@g9v3vnhuuwn4_f+_*'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+TRIPADVISOR_API_KEY = env("TRIPADVISOR_API_KEY")
