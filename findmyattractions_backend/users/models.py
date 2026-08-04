75f4022720dbf2ac5d8c34456c695c45e2107d49
Navid SABETE <nevid.dev@gmail.com>
2026-08-04 17:57:39 +0200

    feat: declare userProfile model in users app

--- findmyattractions_backend/users/models.py
+++ findmyattractions_backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class ProfileType(models.TextChoices):
+    LOCAL = 'Local'
+    TOURISTE = 'Touriste'
+    PRO = 'Professionnel'
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_type = models.CharField(max_length=50, choices=ProfileType.choices)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
