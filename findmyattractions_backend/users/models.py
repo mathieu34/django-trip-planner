1271d3bf6747a4a792aaf052ca4e183160e2ad16
Navid SABETE <nevid.dev@gmail.com>
2026-08-04 18:09:41 +0200

    feat: update userProfile model in users app

--- findmyattractions_backend/users/models.py
+++ findmyattractions_backend/users/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 
 class ProfileType(models.TextChoices):
     LOCAL = 'Local'
@@ -9,9 +7,9 @@ class ProfileType(models.TextChoices):
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    session_key = models.CharField(max_length=40, unique=True)
     profile_type = models.CharField(max_length=50, choices=ProfileType.choices)
     country = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user.username
+        return self.session_key
