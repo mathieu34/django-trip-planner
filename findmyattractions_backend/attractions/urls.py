fa73e3c79e58d10774ba2220231bdbf7e9860f63
Navid SABETE <nevid.dev@gmail.com>
2026-08-05 11:51:13 +0200

    feat: sync local data with api data functions

--- findmyattractions_backend/attractions/urls.py
+++ findmyattractions_backend/attractions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AttractionDetailAPIView
+from .views import AttractionDetailAPIView, SyncAttractionAPIView
 
 urlpatterns = [
     path("<int:pk>/",AttractionDetailAPIView.as_view(), name="attraction_detail"),
+    path("sync/<str:location_id>/", SyncAttractionAPIView.as_view(), name="attraction_sync"),
 ]
--- findmyattractions_backend/attractions/views.py
+++ findmyattractions_backend/attractions/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Attraction
 from .serializers import AttractionSerializer
+from .services.trip_advisor import sync_attraction
 
 # Create your views here.
 
@@ -8,3 +11,10 @@ class AttractionDetailAPIView(RetrieveAPIView):
     """GET /api/attractions/<id>/"""
     queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
+
+
+class SyncAttractionAPIView(APIView):
+    def get(self, request, location_id):
+        attraction = sync_attraction(location_id)
+        serializer = AttractionSerializer(attraction)
+        return Response(serializer.data)
