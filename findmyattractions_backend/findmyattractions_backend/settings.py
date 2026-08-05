acca56338ac8aba49d083b334d407c4175d9e2da
NassimBoufalous <boufalousnassim1@gmail.com>
2026-08-05 12:25:10 +0200

    Fix Urls

--- findmyattractions_backend/findmyattractions_backend/settings.py
+++ findmyattractions_backend/findmyattractions_backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compilation'
+    'compilation',
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
