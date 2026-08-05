e017ea8667945931b7e3367d9264343bf651e51b
Navid SABETE <nevid.dev@gmail.com>
2026-08-05 15:42:32 +0200

    feat(frontend): setup app skeleton

---
+++ findmyattractions_frontend/src/pages/Attraction.jsx
@@ -0,0 +1,44 @@
+export default function Attraction() {
+    return (
+        <div className="pt-24">
+
+
+
+
+            <section className="
+max-w-5xl
+mx-auto
+p-10
+">
+
+
+                <h1 className="
+text-6xl
+font-display
+font-bold
+">
+
+                    Attraction 1
+
+                </h1>
+
+
+                <div className="
+grid
+md:grid-cols-3
+gap-5
+mt-10
+">
+
+                </div>
+
+
+            </section>
+
+
+        </div>
+
+
+    )
+
+}
