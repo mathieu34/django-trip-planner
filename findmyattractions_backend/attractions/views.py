f59fd7e7f18dd0f7daf7d43eb46417eadade3a12
Navid SABETE <nevid.dev@gmail.com>
2026-08-06 10:23:17 +0200

    feat(attractions): filtering by country choice

--- findmyattractions_backend/attractions/views.py
+++ findmyattractions_backend/attractions/views.py
@@ -3,12 +3,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import AttractionSerializer
 from .models import Attraction
+from users.models import UserProfile
 
 
 @api_view(['GET'])
 def attraction_list(request): #django appelle toujours une vue avec l'objet request en premier argument. 
-    """Accueil : les attractions les plus populaires"""
-    attractions = Attraction.objects.all().order_by('-likes')[:10]
+    """Accueil : les attractions les plus populaires, du pays choisi par l'utilisateur"""
+    session_key = request.session.session_key
+    if not session_key:
+        return Response({"detail": "Aucune session"})
+    try:
+        profile = UserProfile.objects.get(session_key=session_key)
+    except UserProfile.DoesNotExist:
+        return Response({"detail": "Profil introuvable"})
+    attractions = Attraction.objects.all().filter(country__iexact=profile.country).order_by('-likes')[:10]
     serializer = AttractionSerializer(attractions, many=True)
     return Response({"attractions": serializer.data})
 
